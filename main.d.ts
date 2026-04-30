551d39cc564b1d77df7372cc575da31f2b9996db
NaughtySora <naughtysora@proton.me>
2026-04-30 22:26:27 +0300

    tests, coverage, refactor ring buffer

--- main.d.ts
+++ main.d.ts
@@ -1,11 +1,3 @@
-
-export class RingBuffer {
-  constructor(size: number);
-  length: number;
-  enqueue(data: any): boolean;
-  dequeue(): any;
-}
-
 export class UnrolledQueue {
   constructor(size: number);
   length: number;
--- test/test.js
+++ test/test.js
@@ -1,89 +1,118 @@
 "use strict";
 
-const assert = require("node:assert");
+const assert = require("node:assert/strict");
 const { it, describe } = require("node:test");
-const { UnrolledQueue, RingBuffer } = require("../main");
+const { RingBuffer } = require("../lib/RingBuffer.js");
+const { UnrolledQueue } = require("../lib/UnrolledQueue.js");
+
 
 describe("RingBuffer", () => {
-
-  it("Adding elements", () => {
+  it('reminder', () => {
     const buffer = new RingBuffer(3);
+    assert.equal(buffer.length, 0);
+    assert.equal(buffer.size, 3);
     buffer.enqueue(1);
     buffer.enqueue(2);
+    assert.equal(buffer.length, 2);
     buffer.enqueue(3);
-    assert.strictEqual(buffer.enqueue(4), false);
+    assert.equal(buffer.enqueue(4), false);
+    assert.ok(buffer.isFull);
+    assert.equal(buffer.dequeue(), 1);
+    assert.equal(buffer.enqueue(4), true);
+    assert.equal(buffer.dequeue(), 2);
+    assert.equal(buffer.dequeue(), 3);
+    assert.equal(buffer.dequeue(), 4);
+    assert.equal(buffer.length, 0);
+    assert.ok(buffer.isEmpty);
+    assert.equal(buffer.dequeue(), undefined);
   });
 
-  it("Getting elements", () => {
-    const buffer = new RingBuffer(3);
+  it('bitwise', () => {
+    const buffer = new RingBuffer(8);
+    assert.equal(buffer.length, 0);
     buffer.enqueue(1);
-    assert.strictEqual(buffer.dequeue(), 1);
-    assert.strictEqual(buffer.dequeue(), null);
     buffer.enqueue(2);
+    assert.equal(buffer.length, 2);
+    assert.equal(buffer.size, 8);
     buffer.enqueue(3);
-    assert.strictEqual(buffer.dequeue(), 2);
-    assert.strictEqual(buffer.dequeue(), 3);
-    assert.strictEqual(buffer.dequeue(), null);
+    buffer.enqueue(4);
+    buffer.enqueue(5);
+    buffer.enqueue(6);
+    buffer.enqueue(7);
+    buffer.enqueue(8);
+    assert.equal(buffer.enqueue(9), false);
+    assert.equal(buffer.dequeue(), 1);
+    assert.equal(buffer.enqueue(9), true);
+    assert.equal(buffer.dequeue(), 2);
+    assert.equal(buffer.dequeue(), 3);
+    assert.equal(buffer.dequeue(), 4);
+    assert.equal(buffer.dequeue(), 5);
+    assert.equal(buffer.dequeue(), 6);
+    assert.equal(buffer.dequeue(), 7);
+    assert.equal(buffer.dequeue(), 8);
+    assert.equal(buffer.dequeue(), 9);
+    assert.equal(buffer.length, 0);
+    assert.equal(buffer.dequeue(), undefined);
   });
 });
 
-describe("UnrolledQueue", () => {
-  it("Initialization", () => {
-    const expected = new RangeError("Buffer size has to be greater than zero");
-    assert.throws(() => new UnrolledQueue(), expected);
-    assert.throws(() => new UnrolledQueue(0), expected);
-    assert.throws(() => new UnrolledQueue(0.22), expected);
-    assert.throws(() => new UnrolledQueue(-1), expected);
-    assert.throws(() => new UnrolledQueue({}), expected);
-  });
+// describe.skip("UnrolledQueue", () => {
+//   it("Initialization", () => {
+//     const expected = new RangeError("Buffer size has to be greater than zero");
+//     assert.throws(() => new UnrolledQueue(), expected);
+//     assert.throws(() => new UnrolledQueue(0), expected);
+//     assert.throws(() => new UnrolledQueue(0.22), expected);
+//     assert.throws(() => new UnrolledQueue(-1), expected);
+//     assert.throws(() => new UnrolledQueue({}), expected);
+//   });
 
-  it("Adding elements", () => {
-    const queue = new UnrolledQueue(3);
-    queue.enqueue(1);
-    queue.enqueue(null);
-    queue.enqueue();
-    queue.enqueue({ test: 42 });
-    queue.enqueue([]);
-    queue.enqueue(new Map([[1, 2]]));
-  });
+//   it("Adding elements", () => {
+//     const queue = new UnrolledQueue(3);
+//     queue.enqueue(1);
+//     queue.enqueue(null);
+//     queue.enqueue();
+//     queue.enqueue({ test: 42 });
+//     queue.enqueue([]);
+//     queue.enqueue(new Map([[1, 2]]));
+//   });
 
-  it("Getting elements", () => {
-    const queue = new UnrolledQueue(3);
-    queue.enqueue(1);
-    assert.strictEqual(queue.length, 1);
-    assert.strictEqual(queue.dequeue(), 1);
-    assert.strictEqual(queue.dequeue(), null);
-    queue.enqueue(2);
-    queue.enqueue(3);
-    assert.strictEqual(queue.length, 2);
-    assert.strictEqual(queue.dequeue(), 2);
-    assert.strictEqual(queue.dequeue(), 3);
-    assert.strictEqual(queue.length, 0);
-    assert.strictEqual(queue.dequeue(), null);
-    const array = [1, 42, "test", new Map(), () => { }];
-    queue.enqueue(array);
-    assert.strictEqual(queue.length, 1);
-    assert.strictEqual(queue.dequeue(), array);
-  });
+//   it("Getting elements", () => {
+//     const queue = new UnrolledQueue(3);
+//     queue.enqueue(1);
+//     assert.strictEqual(queue.length, 1);
+//     assert.strictEqual(queue.dequeue(), 1);
+//     assert.strictEqual(queue.dequeue(), null);
+//     queue.enqueue(2);
+//     queue.enqueue(3);
+//     assert.strictEqual(queue.length, 2);
+//     assert.strictEqual(queue.dequeue(), 2);
+//     assert.strictEqual(queue.dequeue(), 3);
+//     assert.strictEqual(queue.length, 0);
+//     assert.strictEqual(queue.dequeue(), null);
+//     const array = [1, 42, "test", new Map(), () => { }];
+//     queue.enqueue(array);
+//     assert.strictEqual(queue.length, 1);
+//     assert.strictEqual(queue.dequeue(), array);
+//   });
 
-  it("Queue many elements, order", () => {
-    const queue = new UnrolledQueue(2048);
-    const add = 441;
-    for (let i = 0; i < add; i++) queue.enqueue(i);
-    assert.strictEqual(queue.length, add);
-    let i = 0;
-    const get = 33;
-    for (; i < get; i++) {
-      assert.strictEqual(queue.dequeue(), i);
-    }
-    assert.strictEqual(queue.length, add - get);
-    const additional = 300;
-    for (let i = 0; i < additional; i++) queue.enqueue(i);
-    assert.strictEqual(queue.length, add - get + additional);
-    const get2 = 10;
-    for (let j = 0; j < get2; j++) {
-      assert.strictEqual(queue.dequeue(), j + i);
-    }
-    assert.strictEqual(queue.length, add - get + additional - get2);
-  });
-});
+//   it("Queue many elements, order", () => {
+//     const queue = new UnrolledQueue(2048);
+//     const add = 441;
+//     for (let i = 0; i < add; i++) queue.enqueue(i);
+//     assert.strictEqual(queue.length, add);
+//     let i = 0;
+//     const get = 33;
+//     for (; i < get; i++) {
+//       assert.strictEqual(queue.dequeue(), i);
+//     }
+//     assert.strictEqual(queue.length, add - get);
+//     const additional = 300;
+//     for (let i = 0; i < additional; i++) queue.enqueue(i);
+//     assert.strictEqual(queue.length, add - get + additional);
+//     const get2 = 10;
+//     for (let j = 0; j < get2; j++) {
+//       assert.strictEqual(queue.dequeue(), j + i);
+//     }
+//     assert.strictEqual(queue.length, add - get + additional - get2);
+//   });
+// });
